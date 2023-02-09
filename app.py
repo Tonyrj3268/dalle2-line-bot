d4c8f01015edc420ce6070352659f81aa150b994
TonyLin <Tony910319@gmail.com>
2023-02-10 03:12:34 +0800

    test to connect line account

--- app.py
+++ app.py
@@ -9,39 +9,48 @@ from linebot.exceptions import (
 from linebot.models import *
 import json
 import random
+from dalle2 import dalle2
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('YAUo5y4kXJzhPCZzwgIpsURZ1sA/o3+cwUs+R6MAnhetZupjz66OqbP4nHpCPIG49svtg96C98qbuJqKSikCNo6poBNDASB+tzBnX0CstIdAoyJ/GP6ICb8FASUQ9Q7AUBRtIEo0R0VUUVVgh/kMBgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('BdimudProf4VkCVmh0C2jAIz1Q+gHGGjehIDN/aENEHbaER8RW2Lwof7bKDCBRVbi0qTmsTTfz5hZUjaGd/RpKbHJAffiO4NdnnEGINAUbdkVWT67s9ZE5iH6DUzypno/IPqWfZq1nx2zVBeLndLZwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('fe3abf9906458712b249941336225c2a')
+handler = WebhookHandler('5b4a7c9c7d6fc61bc1461e1669ae4c87')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
+
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
+    
     try:
+        print(body, signature)
         handler.handle(body, signature)
+        
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='hi')
-        )
-import os
+    if event.message.text:
+        prompt=event.message.text
+        da = dalle2.dalle2()
+        img_url = da.produce_img(prompt)
+        print(img_url)
+        line_bot_api.reply_message(event.reply_token,[
+            ImageSendMessage(
+                original_content_url=img_url,
+                preview_image_url='https://github.com/Tonyrj3268/temple-test/blob/main/%E8%BE%B2%E6%B0%91%E6%9B%86.png?raw=true'),
+            TextSendMessage(text= prompt)])
+        
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
 
 
 
--- dalle2.py
+++ dalle2.py
@@ -1,29 +1,31 @@
 import os
 import openai
-openai.organization = "org-tHt70UMGlNYxmVgdarPogPhh"
-openai.api_key = "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"
-#openai.Model.list()
-from google.colab.patches import cv2_imshow
+
 import cv2
 import base64
 import numpy as np
-def json_to_img(json, download=False, name='some_image.png'):
-  imgdata = base64.b64decode(json)
-  img = cv2.imdecode(np.frombuffer(imgdata, np.uint8), cv2.IMREAD_COLOR)
-  img = cv2.resize(img, (150, 150))
-  cv2_imshow(img)
-  if download:
-    filename = name  # I assume you have a way of picking unique filenames
-    with open(filename, 'wb') as f:
-        f.write(imgdata)
+class dalle2:
+    def __init__(self):
+        openai.organization = "org-tHt70UMGlNYxmVgdarPogPhh"
+        openai.api_key = "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"
 
-response = openai.Image.create(
-  prompt="a blue shark eating ice-cream",
-  n=1,
-  size="1024x1024",
-  response_format='b64_json'
-)
-json = response['data'][0]['b64_json']
-#image_url = response['data'][0]['url'] 
-#url will expire
-json_to_img(json, download=True, name="shark_eat_icecream.png")
+    def json_to_img(json, download=False, name='some_image.png'):
+        imgdata = base64.b64decode(json)
+        img = cv2.imdecode(np.frombuffer(imgdata, np.uint8), cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (150, 150))
+        if download:
+            filename = name  # I assume you have a way of picking unique filenames
+            with open(filename, 'wb') as f:
+                f.write(imgdata)
+    def produce_img(self,prompt,n=1,size="1024x1024",response_format='url'):
+        
+        response = openai.Image.create(
+            prompt=prompt,
+            n=n,
+            size=size,
+            response_format=response_format
+            )
+        url = response['data'][0]['url']
+        
+        #url = prompt
+        return url
