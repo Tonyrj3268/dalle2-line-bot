026defd4ff7f1d6af6062317e8a28df8514b5382
TonyLin <Tony910319@gmail.com>
2023-03-15 00:48:16 +0800

    remove some not required steps

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-import json
-import random
-import dalle2
-import chatgpt_translate as gpt
+from openai import openai
 import configparser
 app = Flask(__name__)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
@@ -24,17 +15,14 @@ handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
-
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    
     try:
-        print(body, signature)
         handler.handle(body, signature)
         
     except InvalidSignatureError:
         abort(400)
-
+        
     return 'OK'
 
 # 處理訊息
@@ -42,19 +30,18 @@ def callback():
 def handle_message(event):
     if event.message.text:
         prompt=event.message.text
+        openai = openai()
         if prompt[:2]=="翻譯":
-            text = gpt.chatgpt().translate(prompt[3:].replace("\n"," "))
+            text = openai.translate(prompt[3:].replace("\n"," "))
             line_bot_api.reply_message(event.reply_token,[
-                TextSendMessage(text= text)])
-        else:    
-            da = dalle2.dalle2()
-            img_url = da.produce_img(prompt)
+                TextSendMessage(text = text)])
+        else:
+            img_url = openai.produce_img(prompt)
             line_bot_api.reply_message(event.reply_token,[
                 ImageSendMessage(
                     original_content_url=img_url,
-                    preview_image_url='https://github.com/Tonyrj3268/temple-test/blob/main/%E8%BE%B2%E6%B0%91%E6%9B%86.png?raw=true'),
+                    preview_image_url="loading.jpg"),
                 TextSendMessage(text= prompt)])
-        
 
 if __name__ == "__main__":
     app.run()
--- openai.py
+++ openai.py
@@ -1,6 +1,6 @@
 import openai
 import configparser
-class chatgpt:
+class openai:
     def __init__(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
@@ -10,7 +10,7 @@ class chatgpt:
     def translate(self,prompt):
         prompt = "請幫我翻譯以下文本成中文 " + prompt
         response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="gpt-3.5-turbo",
         prompt=prompt,
         max_tokens=1024,
         temperature=0.5
@@ -18,3 +18,12 @@ class chatgpt:
         
         text = response.choices[0].text.split("\n\n")[1]
         return text
+
+    def produce_img(self,prompt):
+        response = openai.Image.create(
+        prompt = prompt
+        n=1,
+        size="1024x1024"
+        )
+        image_url = response['data'][0]['url']
+        return image_url
